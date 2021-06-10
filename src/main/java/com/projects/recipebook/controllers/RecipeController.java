a544a6da53180fd18e9cc17ed10fb3c4c6570f3b
erykk <eryk.pilat@ucdconnect.ie>
2021-06-10 19:50:58 +0100

    Added /viewRecipe

--- src/main/java/com/projects/recipebook/controllers/RecipeController.java
+++ src/main/java/com/projects/recipebook/controllers/RecipeController.java
@@ -8,12 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class RecipeController {
@@ -27,6 +29,19 @@ public class RecipeController {
         return "addRecipe";
     }
 
+    @RequestMapping(value = "/recipes", method = RequestMethod.GET)
+    public String viewRecipes(Model model ){
+        model.addAttribute("recipes", recipeRepository.findAll());
+        return "allRecipes";
+    }
+
+    @RequestMapping(value = "/viewRecipe/{id}", method = RequestMethod.GET)
+    public String viewRecipes(Model model, @PathVariable("id") String id){
+        Optional<Recipe> recipe = recipeRepository.findById(id);
+        recipe.ifPresent(value -> model.addAttribute("recipe", value));
+        return "viewRecipe";
+    }
+
     @RequestMapping(value = "/addRecipe", method = RequestMethod.POST)
     public String addRecipe(Model model, @ModelAttribute("recipeInfo") RecipeInfo recipeInfo) {
         Recipe recipe = new Recipe();
@@ -38,6 +53,8 @@ public class RecipeController {
         model.addAttribute("recipes", recipeRepository.findAll());
         return "allRecipes";
     }
+
+
 
     private Map<String, Amount> parseIngredients(String ingredients){
         Map<String, Amount> map = new HashMap<>();
